f5dd39f70d5b282af73759bf6de21c11481e1818
davindev <vinnie.jeong@kidsnote.com>
2023-02-19 18:21:53 +0900

    chore: webpack 패키지 및 config 파일 추가

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }]
+            ],
+            plugins: ['@babel/plugin-transform-react-jsx']
+          }
+        }
+      }
+    ]
+  }  
+};
